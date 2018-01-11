e0d9379328e2d760d8a53796851e4938e8ba178e
Zouz84 <35114443+Zouz84@users.noreply.github.com>
2018-01-11 20:21:21 +0100

    Projet with Js
    Its done batard !

---
+++ main.js
@@ -0,0 +1,10 @@
+function showDiv() {
+
+   document.getElementById('centre').style.display = "block"; /* Depuis OnClick fait appel a la section "Centre" qui est cachée et affiche le block */
+   document.getElementById('bouton').style.display= "none"; 
+   /*  fait disparaitre le bouton*/
+
+}
+
+setTimeout(function() {
+alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please")} , 10000);
